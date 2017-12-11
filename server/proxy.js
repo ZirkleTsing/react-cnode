f32d317b71918bdbcda3b18ace4c07dfbe6456bd
chang-mac <835702580@qq.com>
2017-12-11 14:27:24 +0800

    proxy api

---
+++ server/proxy.js
@@ -0,0 +1,48 @@
+const axios = require('axios')
+const querystring = require('query-string')
+const BASE_URL = require('./config').baseUrl
+
+module.exports = (req, res, next) => {
+  const path = req.path
+  const user = req.session.user || {} // 从session中取
+  const needAccessToken = req.query.needAccessToken
+
+  if (needAccessToken && !user.accessToken) {
+    res.status(401).send({
+      success: false,
+      msg: 'need login'
+    })
+  }
+  const query = Object.assign({}, req.query, {
+    accesstoken: (needAccessToken && req.method === 'GET') ? user.accessToken : ''
+  })
+  if (query.needAccessToken) delete user.needAccessToken
+
+  axios(`${BASE_URL}${path}`, {
+    method: req.method,
+    params: query,
+    data: querystring.stringify(Object.assign({}, req.body, {
+      accesstoken: (needAccessToken && req.method === 'POST') ? user.accessToken : ''
+    })),
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  })
+    .then(resp => {
+      if (resp.status === 200 && resp.data.success) {
+        res.send(resp.data)
+      } else {
+        res.status(resp.status).send(resp.data)
+      }
+    })
+    .catch(err => {
+      if(err.response) {
+        res.status(500).send(err.response.data)
+      } else {
+        res.status(500).send({
+          success: false,
+          msg: '未知错误'
+        })
+      }
+    })
+}
