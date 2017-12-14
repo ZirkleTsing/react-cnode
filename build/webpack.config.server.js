e8b4c460b7317ea755382a5c031cb4f403706e68
chang-mac <835702580@qq.com>
2017-12-14 11:46:05 +0800

    add env varirables to client

--- build/webpack.config.server.js
+++ build/webpack.config.server.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const merge = require('webpack-merge')
-const webpack = require('webpack')
+// const webpack = require('webpack')
 const baseConfig = require('./webpack.config.base')
 
 const config = merge(baseConfig, {
@@ -11,12 +11,12 @@ const config = merge(baseConfig, {
   output: {
     filename: 'server-entry.js',
     libraryTarget: 'commonjs2'
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.API_BASE': '"http://127.0.0.1:3333"'
-    })
-  ]
+  }
+  // plugins: [
+  //   new webpack.DefinePlugin({
+  //     'process.env.API_BASE': '"http://127.0.0.1:3333"'
+  //   })
+  // ]
 })
 
 module.exports = config
