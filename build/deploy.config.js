161eafe4f01d33eb382c1d239a54cabe53da48b0
chang-mac <835702580@qq.com>
2018-01-07 20:50:19 +0800

    aliyun config

---
+++ build/deploy.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  OssConfig: {
+    region: 'oss-cn-beijing',
+    accessKeyId: 'LTAIwYS5LULykLSx',
+    accessKeySecret: 'xRZpNQLbXMnugd3jxzWpcKr4gluecr',
+    bucket: 'cnode',
+    prefix: 'aliyuncs.com'
+  }
+}
